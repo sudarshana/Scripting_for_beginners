f5f972aca09c8839fe46f74210220d61b063acb8
sudarshana <sudarshana.b@gmail.com>
2022-10-23 00:32:52 -0700

    Create hellouser.sh

---
+++ hellouser.sh
@@ -0,0 +1,9 @@
+!/bin/sh
+
+#A script that takes user input and reads it out
+
+
+say Hello. My name is Alfred. What is your name.;
+echo "Enter your name:";
+read varname;
+say Nice to meet you, $varname. I know we will be friends.;
